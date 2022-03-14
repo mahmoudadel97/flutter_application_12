be6487c3b137eb3486e65333f342bfd6fba125e1
mahmoudadel97 <mahmoudadelwahdan97@gmail.com>
2022-03-14 21:26:35 +0200

    intial commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
-
+// comment for commit
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
